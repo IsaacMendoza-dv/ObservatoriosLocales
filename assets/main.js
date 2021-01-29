f3af592c28fd553627f00efd05facdce1e02f898
IsaacMendoza-dv <ca.isaac.mendoza@gmail.com>
2021-01-28 19:40:19 -0600

    first commit

---
+++ assets/main.js
@@ -0,0 +1,149 @@
+var Observatorios ={
+  "agu":{
+    "estado":"Aguascalientes",
+      "obs":[
+      { "name":"Observatorio Ciudadano de Aguascalientes", "page":"http://www.mexicoforense.org/Observatorio.php" }
+      ]
+  },
+  "bcn":{
+    "estado":"Baja California",
+    "obs":[
+    { "name":"Fundación Educando Conseguimos Paz A.C.", "page":"http://edupaz.org.mx/" },
+    ]
+  },
+  "bcs":{
+    "estado":"Baja California Sur",
+    "obs":[
+    { "name":"Cómo vamos la Paz", "page":"http://edupaz.org.mx/" }
+    ]
+  },
+  //Este observatorio no cuenta con página
+  "cam":{
+    "estado":"Campeche",
+    "obs":[
+    { "name":"Observatorio Ciudadano Seguridad Justicia y Legalidad Carmen", "page":"" }
+    ]
+  },
+  "chp":{
+    "estado":"Chiapas",
+    "obs":[
+    { "name":"Observatorio Ciudadano Chiapas de Chiapas", "page":"http://www.ligalab.mx/occh/" },
+    ]
+  },
+  "chh":{
+    "estado":"Chihuahua",
+    "obs":[
+    { "name":"Observatorio Ciudadano de Prevención, Seguridad y Justicia de Chihuahua", "page":"https://ficosec.org/" },
+    { "name":"Observatorio Ciudadano Chiapas de Ciudad Juarez", "page":"http://observatoriochihuahua.org/regiones/juarez" },
+    { "name":"Observatorio Ciudadano de Cuauhtémoc A.C.", "page":"http://observatoriochihuahua.org/regiones/juarez" },
+    { "name":"Observatorio Ciudadano de Seguridad y Justicia de Nuevo Casas Grandes", "page":"https://t.ly/eR69R" },
+    { "name":"Observatorio Ciudadano de Parral", "page":"https://t.ly/6w8rx" }
+    ]
+  },
+  "dur":{
+    "estado":"Durango",
+    "obs":[
+    { "name":"Observatorio Ciudadano de Durango", "page":"https://observatoriodgo.org/" }
+    ]
+  },
+  "gua":{
+    "estado":"Guanajuato",
+    "obs":[
+    { "name":"Observatorio Ciudadano de León", "page":"http://www.ligalab.mx/occh/" },
+    { "name":"Irapuato Cómo Vamos", "page":"https://t.ly/3wrZ2" }
+    ]
+  },
+  //Aclaración de color, está en naranja
+  "jal":{
+    "estado":"Jalisco",
+    "obs":[
+    { "name":"", "page":"" }
+    ]
+  },
+  "cmx":{
+    "estado":"Ciudad de México",
+    "obs":[
+    { "name":"Observatorio Ciudadano de la Ciudad de México", "page":"https://onc.org.mx/ocmx/3-t-2020/" }
+    ]
+  },
+  "mex":{
+    "estado":"Estado de México",
+    "obs":[
+    { "name":"Observatorio Ciudadano de Seguridad Maninalco", "page":"http://imaginalco.org/es_ES/" },
+    { "name":"Laboratorio de Seguridad Ciudadana: Estado de México", "page":"https://onc.org.mx/lab-seguridad-edomex/3-t-2020/" },
+    ]
+  }
+  "mic":{
+    "estado":"Michoacán",
+    "obs":[
+    { "name":"Observatorio Ciudadano de Michoacán", "page":"http://observatoriomichoacan.com/" },
+    { "name":"Observatorio Regional Zamora", "page":"https://t.ly/2w7Bb" },
+    { "name":"Observatorio de Seguridad Humana de la Región de Apatzingán", "page":"https://www.ocapatseguridad.org/" }
+    ]
+  },
+  "mor":{
+    "estado":"Morelos",
+    "obs":[
+    { "name":"Observatorio Ciudadano de Justicia y Legalidad de Morelos", "page":"http://causaencomun.org.mx/beta/el-observatorio-ciudadano-de-justicia-y-legalidad-de-morelos-participara-en-la-rutahuffpost/" },
+    ]
+  },
+  "nle":{
+    "estado":"Nuevo León",
+    "obs":[
+    { "name":"Observatorio Ciudadano de Nuevo León", "page":"http://www.consejocivico.org.mx/" },
+    { "name":"Observatorio de Seguridad y Justicia", "page":"https://observatorio.conl.mx/" },
+    ]
+  },
+  "oax":{
+    "estado":"Oaxaca",
+    "obs":[
+    { "name":"Observatorio Ciudadano de Oaxaca de Fundación ICSUS A.C.", "page":"http://causaencomun.org.mx/beta/el-observatorio-ciudadano-de-justicia-y-legalidad-de-morelos-participara-en-la-rutahuffpost/" },
+    ]
+  },
+  "pue":{
+    "estado":"Puebla",
+    "obs":[
+    { "name":"Observatorio Ciudadano de Puebla, A.C.", "page":"http://puebla.cic.mx/" },
+    { "name":"Instituto para la Gestión, Administración y Vinculación Municipal", "page":"http://igavim.org/" }
+    ]
+  },
+  "roo":{
+    "estado":"Quintana Roo",
+    "obs":[
+    { "name":"Observatorio de Seguridad y Género de Quintana Roo", "page":"https://www.facebook.com/OSEGEMX" },
+    ]
+  },
+  "son":{
+    "estado":"Sonora",
+    "obs":[
+    { "name":"Observatorio Sonora por la Seguridad", "page":"https://www.observatoriosonoraporlaseguridad.org/" },
+    ]
+  },
+  "tab":{
+    "estado":"Tabasco",
+    "obs":[
+    { "name":"Observatorio Ciudadano Tabasco", "page":"http://octabasco.com/" },
+    ]
+  },
+  "tam":{
+    "estado":"Tamaulipas",
+    "obs":[
+      //Presenta error Su página
+    { "name":"Observatorio Ciudadano Tamaulipas", "page":"http://www.octma.org.mx/" },
+    { "name":"Observatorio del Sur de Tamaulipas", "page":"https://twitter.com/ObservatorioTMA" },
+    ]
+  },
+  "ver":{
+    "estado":"Veracruz",
+    "obs":[
+    { "name":"Observatorio Ciudadano Veracruz - Boca del Río", "page":"https://twitter.com/ocvb_seguridad" },
+    { "name":"Observatorio Ciudadano Coatzacoalcos", "page":"http://www.coatzaobserva.org/v2/" },
+    ]
+  },
+  "son":{
+    "estado":"Sonora",
+    "obs":[
+    { "name":"Observatorio Sonora por la Seguridad", "page":"https://www.observatoriosonoraporlaseguridad.org/" },
+    ]
+  },
+}
